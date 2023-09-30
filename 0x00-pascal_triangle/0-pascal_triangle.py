1c5a2342bf3dc992b864cbb3bf9dcbb4568c938e
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-09-30 23:16:18 +0100

    Pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal_triangle(n):
+    """ Function to develop pascal triangle"""
+    triangle = []
+    i = 0   # Initialize row index
+    if (n <= 0):
+        return []
+    while i < n:
+        row = [1]   # Start each row with 1
+        if triangle:   # If not the first row
+            prev_row = triangle[-1]
+            for j in range(len(prev_row) - 1):
+                row.append(prev_row[j] + prev_row[j + 1])
+            row.append(1)  # End each row with 1
+        triangle.append(row)
+        i += 1
+
+    return triangle
